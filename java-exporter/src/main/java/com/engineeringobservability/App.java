ed6e5e13b7b2b52b8410cfc65873055b17849159
Mike Rae <mike.rae.name@outlook.com>
2026-06-05 15:35:19 +0100

    Create Java exporter skeleton

---
+++ java-exporter/src/main/java/com/engineeringobservability/App.java
@@ -0,0 +1,40 @@
+package com.engineeringobservability;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
+import com.sun.net.httpserver.HttpServer;
+
+import io.prometheus.metrics.exporter.httpserver.HTTPServer;
+
+public class App {
+    public static void main(String[] args) throws IOException {
+        startHealthServer();
+
+        HTTPServer metricsServer = HTTPServer.builder()
+                .port(2113)
+                .buildAndStart();
+
+        System.out.println("java-exporter running");
+        System.out.println("health endpoint:  http://localhost:8081/health");
+        System.out.println("metrics endpoint: http://localhost:2113/metrics");
+
+        Runtime.getRuntime().addShutdownHook(new Thread(metricsServer::close));
+    }
+
+    private static void startHealthServer() throws IOException {
+        HttpServer server = HttpServer.create(new InetSocketAddress(8081), 0);
+
+        server.createContext("/health", exchange -> {
+            String response = "OK\n";
+            exchange.sendResponseHeaders(200, response.length());
+
+            try (OutputStream os = exchange.getResponseBody()) {
+                os.write(response.getBytes());
+            }
+        });
+
+        server.start();
+    }
+}
